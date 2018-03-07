98a5181903da593f5233c17560910a6b8c386a14
Abhinav Peddi <p.abhinav3@gmail.com>
2018-03-07 19:57:31 +0530

    Add files via upload

---
+++ switch.ino
@@ -0,0 +1,43 @@
+#include<Servo.h>
+Servo myservoY; // Roll
+String myString;
+int incomingByte=0;
+void setup() {
+  
+    myservoY.attach(10); // Attach Y servo to pin 9
+  // put your setup code here, to run once:
+Serial.begin(115200);
+myservoY.write(90);
+/*myString=Serial.read();
+if(myString==1)
+{
+  myservoY.write(180); 
+}
+else if(myString==2)
+{
+  myservoY.write(0); 
+}*/
+}
+
+void loop() {
+  if (Serial.available() > 0) /*{
+                // read the incoming byte:
+                incomingByte = Serial.read();
+
+                // say what you got:
+                Serial.print("I received: ");
+                Serial.println(incomingByte);
+        }*/
+        {
+          myString=Serial.readString();
+if(abs(myString.toInt())==1)
+{
+ myservoY.write(125);
+}
+else if(abs(myString.toInt())==2)
+{
+  myservoY.write(55); 
+}
+        }
+
+}
